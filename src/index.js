18bb0555f893f4685e5fa7568e91ec5649bfa2d9
Замалдинов Равиль <developer@kirano.uz>
2025-03-09 00:27:50 +0500

    upd

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import commands from './commands/index.js'
 
 const args = process.argv.slice(2);
-const command = args[0].split(':')[0];
+const command = args[0]?.split(':')[0];
 
 if(!command) commands.help()
 else if(!commands[command]) commands.unknownCommand()
