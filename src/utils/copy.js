bae3b872e4c8d8614a65b1931a87b088068e5b93
kirano-dev <developer@kirano.uz>
2025-03-11 14:57:22 +0500

    upd

--- src/utils/copy.js
+++ src/utils/copy.js
@@ -1,22 +1,5 @@
 import * as fs from "node:fs";
-import path from "node:path";
 
 export default function copy(sourceDir, targetDir) {
-	const items = fs.readdirSync(sourceDir, { withFileTypes: true });
-	
-	items.forEach((item) => {
-		const sourcePath = path.join(sourceDir, item.name);
-		const targetPath = path.join(targetDir, item.name);
-		
-		const stat = fs.statSync(sourcePath);
-		
-		if (stat.isDirectory()) {
-			if (!fs.existsSync(targetPath)) {
-				fs.mkdirSync(targetPath, { recursive: true });
-			}
-			copy(sourcePath, targetPath);
-		} else if (stat.isFile()) {
-			fs.copyFileSync(sourcePath, targetPath);
-		}
-	});
+	fs.cpSync(sourceDir, targetDir, {recursive: true});
 };
